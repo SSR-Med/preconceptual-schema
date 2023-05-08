b515005f20cee375f931e455b33eddb5e548b716
SSR-Med <sasalazarr@unal.edu.co>
2023-05-08 17:08:28 -0500

    Update Additional.jsx

--- src/Additional.jsx
+++ src/Additional.jsx
@@ -6,6 +6,16 @@ export default function Additional() {
             <div className="content">
                 <div className='context-text'>
                     <h1>Additional Content</h1>
+                    <p>Books:</p>
+                    <ul>
+                        <li>Jaramillo, C. (2012). The UNC-Method revisited: elements of the new approach: Eliciting software requirements in a complete, consistent, and correct way. https://www.amazon.com/UNC-Method-revisited-Eliciting-requirements-consistent/dp/3848407590</li>
+                        <li>Jaramillo, C., Gelbukh, A., & Isaza, F. A. (2006). Pre-conceptual Schema: A Conceptual-Graph-Like Knowledge Representation for Requirements Elicitation. En Springer eBooks (pp. 27-37). https://doi.org/10.1007/11925231_3</li>
+                    </ul>
+                    <p>Videos:</p>
+                    <ul>
+                        <li>Solving logic problems with pre-conceptual schemas: https://www.youtube.com/watch?v=J2XwPxBF_1g</li>
+                        <li>How you can represent a preconceptual schema in CASE tools: https://youtu.be/hb4aVWzyUFM</li>
+                    </ul>
                 </div>
             </div>
             <div id="vl">
